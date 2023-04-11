b8114ec58b7486ae9848fe5d7806dd4eead29c52
Joris Noordermeer <joris@noordermeer.ch>
2023-04-11 15:03:53 +0700

    feat: seo preview module

--- src/SeoPreview.php
+++ src/SeoPreview.php
@@ -2,28 +2,27 @@
 namespace jorisnoo\CraftModules;
 
 use Craft;
-use craft\console\controllers\InvalidateTagsController;
+use craft\base\Element;
+use craft\events\RegisterPreviewTargetsEvent;
+use craft\events\RegisterTemplateRootsEvent;
 use craft\events\RegisterUrlRulesEvent;
 use craft\helpers\UrlHelper;
 use craft\web\UrlManager;
-use yii\base\ActionEvent;
+use craft\web\View;
 use yii\base\Event;
 use yii\base\Module;
-use craft\base\Element;
-use craft\events\RegisterPreviewTargetsEvent;
 
 class SeoPreview extends Module
 {
     public function init()
     {
-
-        Craft::setAlias('@modules', __DIR__);
+        Craft::setAlias('@jorisnoo/craft-modules', __DIR__);
 
         // Set the controllerNamespace based on whether this is a console or web request
         if (Craft::$app->request->isConsoleRequest) {
-            $this->controllerNamespace = 'modules\\console\\controllers';
+            $this->controllerNamespace = 'jorisnoo\\CraftModules\\console\\controllers';
         } else {
-            $this->controllerNamespace = 'modules\\controllers';
+            $this->controllerNamespace = 'jorisnoo\\CraftModules\\controllers';
         }
 
         parent::init();
@@ -31,13 +30,12 @@ class SeoPreview extends Module
         // Defer most setup tasks until Craft is fully initialized
         Craft::$app->onInit(function() {
             $this->attachEventHandlers();
-            // ...
         });
-
     }
 
     private function attachEventHandlers(): void
     {
+        $request = Craft::$app->getRequest();
 
         // Register preview target
         Event::on(
@@ -58,25 +56,27 @@ class SeoPreview extends Module
             }
         );
 
+        // Register preview site route
+        if (!Craft::$app->user->isGuest && $request->getIsSiteRequest() && !$request->getIsConsoleRequest()) {
+
+            Event::on(
+                View::class,
+                View::EVENT_REGISTER_SITE_TEMPLATE_ROOTS,
+                static function(RegisterTemplateRootsEvent $event) {
+                    $event->roots['_jorisnoo'] = __DIR__ . '/templates';
+                }
+            );
+
+            Event::on(
+                UrlManager::class,
+                UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+                static function (RegisterUrlRulesEvent $event) {
+                    $event->rules['seopreview/preview'] = [
+                        'template' => '_jorisnoo/seo',
+                    ];
+                }
+            );
+        }
     }
-
-
-//        Craft::setAlias('@modules', __DIR__);
-//        parent::init();
-//
-//
-
-//
-//        // Register preview site route
-//        $request = Craft::$app->getRequest();
-//        if ($request->getIsSiteRequest() && !$request->getIsConsoleRequest()) {
-//            Event::on(
-//                UrlManager::class,
-//                UrlManager::EVENT_REGISTER_SITE_URL_RULES,
-//                static function(RegisterUrlRulesEvent $event) {
-//                    $event->rules['seopreview/preview'] = ['template' => '_previews/seo'];
-//                }
-//            );
-//        }
 
 }
