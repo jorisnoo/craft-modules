e06f0ce3e508d0265b4a43a9f080630a6c624a33
Joris Noordermeer <joris@noordermeer.ch>
2026-04-04 17:52:22 +0800

    feat: integrate ohdear queue health check error handling

--- src/CraftModules.php
+++ src/CraftModules.php
@@ -3,7 +3,10 @@
 namespace Noo\CraftModules;
 
 use Craft;
+use craft\queue\Queue;
+use yii\base\Event;
 use yii\base\Module;
+use yii\queue\ExecEvent;
 
 class CraftModules extends Module
 {
@@ -26,6 +29,18 @@ class CraftModules extends Module
         Craft::$app->getModule('remote-sync');
         Craft::$app->getModule('twig-helpers');
 
+
+        if (class_exists(\webhubworks\ohdear\health\jobs\QueueHealthJob::class)) {
+            Event::on(
+                Queue::class,
+                Queue::EVENT_AFTER_ERROR,
+                function (ExecEvent $event) {
+                    if ($event->job instanceof \webhubworks\ohdear\health\jobs\QueueHealthJob) {
+                        Craft::$app->getQueue()->release($event->id);
+                    }
+                }
+            );
+        }
 
         Craft::$app->onInit(function () {
             $modules = [
